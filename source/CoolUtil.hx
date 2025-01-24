0230b1e6b3f087a732e0db8c9346830279d4a9c9
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-24 23:37:07 +0800

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -71,6 +71,16 @@ class CoolUtil
 
 		return listFromString(rawList);
 	}
+	public static function colorFromString(color:String):FlxColor
+	{
+		var hideChars = ~/[\t\n\r]/;
+		var color:String = hideChars.split(color).join('').trim();
+		if(color.startsWith('0x')) color = color.substring(color.length - 6);
+
+		var colorNum:Null<FlxColor> = FlxColor.fromString(color);
+		if(colorNum == null) colorNum = FlxColor.fromString('#$color');
+		return colorNum != null ? colorNum : FlxColor.WHITE;
+	}
 	public static function listFromString(string:String):Array<String>
 	{
 		var daList:Array<String> = [
@@ -173,6 +183,15 @@ class CoolUtil
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else
 		flixel.FlxG.openURL(site);
+		#end
+	}
+
+	public static function showPopUp(message:String, title:String):Void
+	{
+		#if android
+		android.Tools.showAlertDialog(title, message, {name: "OK", func: null}, null);
+		#else
+		FlxG.stage.window.alert(message, title);
 		#end
 	}
 }
