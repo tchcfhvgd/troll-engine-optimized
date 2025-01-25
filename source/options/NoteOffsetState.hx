621903cf99f5966d45525a0cb558f2996af281a9
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-25 17:08:10 +0800

    Update NoteOffsetState.hx

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -497,6 +497,7 @@ class NoteOffsetState extends MusicBeatState
 		timeTxt.text = 'Current offset: ' + Math.floor(barPercent) + ' ms';
 	}
 
+	final buttonAccept:String = controls.mobileC ? 'A' : 'Accept';
 	function updateMode()
 	{
 		rating.visible = onComboMenu;
@@ -509,11 +510,14 @@ class NoteOffsetState extends MusicBeatState
 		timeTxt.visible = !onComboMenu;
 		beatText.visible = !onComboMenu;
 
-		if(onComboMenu)
-			changeModeText.text = '< Combo Offset (Press Accept to Switch) >';
-		else
-			changeModeText.text = '< Note/Beat Delay (Press Accept to Switch) >';
+		addTouchPad(onComboMenu ? "NONE" : "LEFT_RIGHT", "A_B_C");
+		addTouchPadCamera();
 
+		if(onComboMenu)
+			changeModeText.text = '< Combo Offset (Press $buttonAccept to Switch) >';
+		else
+			changeModeText.text = '< Note/Beat Delay (Press $buttonAccept to Switch) >';
+		
 		changeModeText.text = changeModeText.text.toUpperCase();
 		FlxG.mouse.visible = onComboMenu;
 	}
