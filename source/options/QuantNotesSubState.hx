8e5ef18cd090b5eec2e865976d7c2a487c9f6377
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-25 17:09:46 +0800

    Update QuantNotesSubState.hx

--- source/options/QuantNotesSubState.hx
+++ source/options/QuantNotesSubState.hx
@@ -115,6 +115,8 @@ class QuantNotesSubState extends MusicBeatSubstate
 
 		changeSelection();
 		cameras = [daCam];
+
+		addTouchPad("LEFT_FULL", "A_B_C");
 	}
 
 	var changingNote:Bool = false;
@@ -127,7 +129,7 @@ class QuantNotesSubState extends MusicBeatSubstate
 				} else if(controls.UI_RIGHT_P) {
 					updateValue(1);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
-				} else if(controls.RESET) {
+				} else if(controls.RESET || touchPad.buttonC.justPressed) {
 					resetValue(curSelected, typeSelected);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 				}
@@ -168,7 +170,7 @@ class QuantNotesSubState extends MusicBeatSubstate
 				changeType(1);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
-			if(controls.RESET) {
+			if(controls.RESET || touchPad.buttonC.justPressed) {
 				for (i in 0...3) {
 					resetValue(curSelected, i);
 				}
