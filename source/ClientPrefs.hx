49811b2a5317e0ebd0d829ccfa8497b2c59bc78a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-26 19:52:41 +0800

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -584,7 +584,7 @@ class ClientPrefs
 				display: "Antialiasing",
 				desc: "When toggled, sprites are able to be antialiased.",
 				type: Toggle,
-				value: false,
+				value: true,
 				data: []
 			},
 			"multicoreLoading" => {
